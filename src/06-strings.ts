473d1e2ea943206816afb96030eebbd5908c8e45
Fabian Ardila <fabianra97@gmail.com>
2022-04-12 14:50:06 -0500

    string type class

---
+++ src/06-strings.ts
@@ -0,0 +1,17 @@
+(() => {
+  let productTitle = 'My amazing product';
+  productTitle = ' My amazing product changed';
+  console.log('productTitle', productTitle);
+
+  const productDescription = 'ahsbviwqbe vi';
+  console.log('productDescription', productDescription);
+
+  const summary = `
+    eqgewrg
+    wetrgwerg
+    wergqe4rf
+    q34rg4rg
+    ${productTitle}
+    ${productDescription}
+  `;
+})();
